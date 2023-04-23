b3e08a201e4fc703b0e9b4a7c0437b70c6f4529e
DawnSingularity <70613207+DawnSingularity@users.noreply.github.com>
2023-04-23 21:08:37 +0800

    Profile page with ssg helper

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,21 +1,10 @@
 import { z } from "zod";
-import type{ User } from "@clerk/nextjs/dist/api";
 import { createTRPCRouter, privateProcedure, publicProcedure } from "y/server/api/trpc";
 import { clerkClient } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
-
-
-
-const filterUserForClient = (user: User) => {
-    return {
-        id: user.id, 
-        username: user.username, 
-        profileImageUrl: user.profileImageUrl,
-    };
-};
-
 import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
 import { Redis } from "@upstash/redis";
+import { filterUserForClient } from "y/server/helpers/filterUserForClient";
 
 // Create a new ratelimiter, that allows 3 requests per 1 minute
 const ratelimit = new Ratelimit({
